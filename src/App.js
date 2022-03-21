d0f2b4f6ccf387c64d7b9da24d354bd40952fab5
ReflextionsDev <reflextionsdev@gmail.com>
2022-03-21 12:15:03 -0400

    App setup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello there.</p>
+
+      <header>Bug Tracker</header>
+
+      <main>
+
+        <div className='Items'>
+          <div className='Bug'>Bug</div>
+        </div>
+
+        <div className='Editor'>New Bug</div>
+
+      </main>
     </div>
+
   );
 }
 
